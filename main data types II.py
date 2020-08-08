9666cff9b8d1ddd6f87ad760e748bb68e27fe440
Jonat-D <68782317+Jonat-D@users.noreply.github.com>
2020-08-07 23:18:35 -0400

    Add files via upload
    Algunos apuntes para principiantes en el Lenguaje PYTHON 3

---
+++ main data types II.py
@@ -0,0 +1,154 @@
+#Built-in Functions /  Funciones integradas
+print(x, sep='y') #prints x objects separated by y / imprime x objetos separados por y
+
+input(s) #prints s and waits for an input that will be returned / imprime sy espera una entrada que será devuelta
+
+len(x) #returns the length of x (s, L or D) / Devuelve la longitud de x (s, L o D)
+
+min(L) #returns the minimun value in L / devuelve el valor mínimo en L
+
+max(L) #returns the maximun values in L / devuelve el valor maximo en L
+
+sum(L) #returns the sum of the values in L / devuelve la suma de los valores en L
+
+range(n1,n2,n3) #returns a sequence of numbers from n1 to n2 in steps of n / returns a sequence of numbers from n1 to n2 in steps of n
+
+abs(n) #returns the absolute value of n / devuelve el valor absoluto de n
+
+round(n1,n) #returns the n1 number rounded to n digits / devuelve el número n1 redondeado a n dígitos
+
+type(x) #returns the type of x (string, float,list,dict...) / devuelve el tipo de x (string, float, list, dict ...)
+
+str(x) #converts x to a string / convierte x en una cadena
+
+list(x) #converts x to a list / convierte x en una lista
+
+int(x) #converts x to a integer / convierte x en entero
+
+float(x) #converts x to float number / convierte x en número flotante
+
+help(s) #prints help about x / imprime ayuda sobre x
+
+map(function, L) #applies function to values in L / aplica la función a los valores en L
+
+#Conditionals statem,ents / Declaraciones condicionales
+
+if <condition>:
+    <code>
+    else if<condition>
+        <code>
+    ...
+    else:
+        <code>
+
+if<value>in<list>:
+
+#Data validation / Validacion de datos
+try:
+    <code>
+except<error>:
+    <code>
+else:
+    <code>
+
+#Working with files and folders / Trabajar con archivos y carpetas
+import os
+os.getcwd()
+os.makedirs(<path>)
+os.chdir(<path>)
+os.listdir(<path>)
+
+#Loops / Bucles
+while<condition>:
+    <code>
+
+for<variable>in<list>:
+    <code>
+
+for<variable>in
+    range(start,stop,step):
+    <code>
+for key, value in
+    dict.tems():
+    <code>
+
+#Loop Control statements / Declaraciones de control de bucle
+break #finishes loop / termina lazo
+
+continue #jumps to next iteration / salta a la siguiente iteración
+
+pass #does nothing / no hace nada
+
+#running externals programs / ejecutar programas externos
+import os
+os.system(<command>)
+
+#Functions / Las funciones
+def function(<params>):
+    <code>
+    return<data>
+
+#Modules / Modulos
+import module
+module.function()
+
+from module import*
+function()
+
+#reading and writing files / leer y escribir archivos
+f=open(<path>,'r')
+f.read(<size>)
+f.readline(<size>)
+f.close()
+
+f=open(<path>,'r')
+for line in f:
+    <code>
+f.close()
+f=open(<path>,'W')
+f.write(<str>)
+f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main data types.py
@@ -0,0 +1,187 @@
+#Main Data Types / PRINCIPALES TIPOS DE DATOS
+
+boolean = True / False
+
+integer = 10
+
+float = 10.01
+
+string = "123abc"
+
+list = ["value1,value2"]
+
+dictionary = {"key1:value1","key2:value2"}
+
+#Numeric Operators /OPERADORES NUMERICOS
+
+'''addition + 
+
+subtraction -
+
+multiplication*
+
+division /
+
+exponent **
+
+modulus %
+
+floor division //
+'''
+
+#Comparison Operators / OPERADORES DE COMPARACION
+'''
+
+equal ==
+
+different !=
+
+higher >
+
+lower <
+
+higher or equal >=
+
+lower or equal <=
+'''
+
+#Boolean Operators / OPERADORES BOOLEANOS
+'''
+
+Logical AND and
+
+Logical OR or
+
+Logical NOT not
+'''
+
+#Special characters / CARACTERES ESPECIALES
+'''
+
+comment #
+
+new line \n
+
+scape char \<char>
+'''
+#strings operations / Operaciones de cadenas
+
+string[i] #retrieves character at position i /recupera el caracter de la posicion i
+
+string [-1] #retrieves last character  /recupera el ultimo caracter
+
+string [i:j] #retrieves characterer in range i to j /recupera el caracter del rango i hasta j
+
+#List operations / Operaciones de listas
+
+list=[] #Define una lista vacia
+
+list[i]=x #almacena x con el indice i
+
+list[i] #recupera el item con el indice i
+
+list[-1] #recupera el ultimo item
+
+list[i:j] #recupera los items en el rango de i a j
+
+del list[i] #remueve de la lista el item con el indice i
+
+#Dictionary Operations / Operciones con Diccionario
+
+dict = {} #Define un Diccionario Vacio
+
+dict[k] = x #Almacena x asociado a la clave k
+
+del dict[k] #remueve el item con la clave k
+
+#Strings Methods / Metodos de cadenas
+
+string.upper() #Converts to uppercase / Convierte a mayúsculas
+
+string.lower() #Converts to lowercase / Convierte a minuscula
+
+string.count(x) #counts how many times x appears / cuenta cuántas veces aparece x
+
+string.find(x) #position of the x firts occurrence / posición de la primera ocurrencia x
+
+string.replace(x,y) #replace x for y / reemplazar x por y
+
+string.strip(x) #returns a list of values delimited by x / devuelve una lista de valores delimitados por x
+
+string.join(L) #returns a string with L values joined by string /  devuelve una cadena con valores L unidos por cadena
+
+string.format(x) #returns a string that includes formatted x / devuelve una cadena que incluye x formateada
+
+#List Methods / Metodos de listas
+
+list.append(x) #adds x to the end of the list / agrega x al final de la lista
+
+list.extend(L) #appens L to the end of the list / aparece L al final de la lista
+
+list.insert(i,x) #inserts x a i position / inserta x una posición i
+
+list.remove(x) #removes the firts list item whose value is x / elimina el primer elemento de la lista cuyo valor es x
+
+list.pop(i) #removes the item at position i and returns its value / elimina el elemento en la posición i y devuelve su valor
+
+list.clear() #removes all the item from the list elimina t odo el elementos de la lista
+
+list.index() #returns a list of values delimited by x / devuelve una lista de valores delimitados por x
+
+list.count(x) #returns a string with list values joined by S / devuelve una cadena con valores de lista unidos por S
+
+list.sort() #sort list items / ordenar elementos de la lista
+
+list.reverse() #reverse list elements / elementos de la lista inversa
+
+list.copy() #returns a copy of the list / devuelve una copia de la lista
+
+#Dictionary Methods / Metodos de diccionario
+
+dict.keys() #returns a list of keys / devuelve una lista de claves
+
+dict.values() #returns a list of values / devuelve una lista de valores
+
+dict.items() #returns a list of pairs (key,value) / devuelve una lista de pares (clave, valor)
+
+dict.get(k) #returns the value associated to the key and returns its value / devuelve el valor asociado a la clave y devuelve su valor
+
+dict.pop() #removes the item associated to the key and returns its value / elimina el elemento asociado a la clave y devuelve su valor
+
+dict.update(D) #adds keys-values to the dictionary / agrega clave-valores al diccionario
+
+dict.clear() #removes all keys-values from the dictionary / elimina todas las claves-valores del diccionario
+
+dict.copy() #returns a copy of the dictionary / devuelve una copia del diccionario
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
